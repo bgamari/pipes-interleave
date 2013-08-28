97154aa0fef5edc709fcd2e80699d664ae98649e
Ben Gamari <bgamari.foss@gmail.com>
2013-08-28 14:19:59 -0400

    Add haddocks

--- Pipes/Interleave.hs
+++ Pipes/Interleave.hs
@@ -9,8 +9,17 @@ import Data.Function (on)
 import Data.Either (rights)
 import Pipes
 
+-- $setup
+-- >>> import Pipes.Prelude
+-- >>> import Data.Function
+-- >>> import Data.Functor.Identity
+
 -- | Interleave elements from a set of 'Producers' such that the interleaved
--- stream is increasing with respect to the given ordering
+-- stream is increasing with respect to the given ordering.
+-- 
+-- >>> toList $ interleave compare [each [1,3..10], each [1,5..20]] 
+-- [1,1,3,5,5,7,9,9,13,17]
+-- 
 interleave :: (Monad m, Functor m)
            => (a -> a -> Ordering) -> [Producer a m ()] -> Producer a m ()
 interleave compare producers = do
@@ -25,6 +34,11 @@ interleave compare producers = do
 
 -- | Given a stream of increasing elements, combine those equal under the 
 -- given equality relation
+--
+-- >>> let append (k,v) (_,v') = return (k, v+v')
+-- >>> toList $ combine ((==) `on` fst) append (each [(1,1), (1,4), (2,3), (3,10)])
+-- [(1,5),(2,3),(3,10)]
+--
 combine :: (Monad m)
         => (a -> a -> Bool)    -- ^ equality test
         -> (a -> a -> m a)     -- ^ combine operation
@@ -40,6 +54,12 @@ combine eq append producer = lift (next producer) >>= either return (uncurry go)
               | otherwise          -> yield a >> go a' producer''
    
 -- | Equivalent to 'combine' composed with 'interleave'
+--
+-- >>> let append (k,v) (_,v') = return (k, v+v')
+-- >>> let producers = [ each [(i,2) | i <- [1,3..10]], each [(i,10) | i <- [1,5..20]] ] :: [Producer (Int,Int) Identity ()]
+-- >>> toList $ merge (compare `on` fst) append producers
+-- [(1,12),(3,2),(5,12),(7,2),(9,12),(13,10),(17,10)]
+-- 
 merge :: (Monad m, Functor m)
       => (a -> a -> Ordering) -> (a -> a -> m a)
       -> [Producer a m ()] -> Producer a m ()
